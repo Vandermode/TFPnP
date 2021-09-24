6e65a9e7d4748788a35ba8e96f1ee634dd886135
zeqiang-lai <laizeqiang@outlook.com>
2021-09-24 16:11:15 +0800

    update training script

--- tasks/csmri/script.sh
+++ tasks/csmri/script.sh
@@ -4,6 +4,4 @@
 # python -W ignore main.py --solver apg --exp test -rs 15000 --max_episode_step 6 --action_pack 5 --eval -r ./checkpoints/csmri_apg_5x6_48/actor_0015000.pkl
 # python -W ignore main.py --solver redadmm --exp test -rs 15000 --max_episode_step 6 --action_pack 5 --eval -r ./checkpoints/csmri_red_5x6_48/actor_0015000.pkl
 
-python -W ignore main.py --solver admm --exp debug --validate_interval 50 --env_batch 12 --rmsize 10 --warmup 20 -lp 0.05 --train_steps 15000 --max_episode_step 6 --action_pack 5 -le 0.2
-
-python train_csmri.py --solver admm --exp csmri_admm_5x6_48_new --validate_interval 50 --env_batch 48 --rmsize 480 --warmup 20 -lp 0.05 --train_steps 15000 --max_episode_step 6 --action_pack 5 -le 0.2
+python -W ignore main.py --solver admm --exp csmri_admm_5x6_48_new --validate_interval 10 --env_batch 48 --rmsize 480 --warmup 20 -lp 0.05 --train_steps 15000 --max_episode_step 6 --action_pack 5 -le 0.2
